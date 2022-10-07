5d18d05d9eccef10daa7a9923ccfac159914e5aa
rebplane <rebecca_nhan@rogers.com>
2022-10-07 16:27:48 -0400

    Converted our old .csv files provided by partner to new .csv files so that it's easier to convert into a database. Stripped unnecessary brackets. Each exercise is now linked to its respective book.

---
+++ database_conversion/Book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, book_id, author, date, title, link):
+        self.book_id = book_id
+        self.author = author
+        self.date = date
+        self.title = title
+        self.link = link
---
+++ database_conversion/Exercise.py
@@ -0,0 +1,4 @@
+class Exercise:
+    def __init__(self, exercise_id, tag_id):
+        self.exercise_id = exercise_id
+        self.tag_id = tag_id
