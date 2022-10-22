73c5b9876ceaf0997871946a8458785777d51bb1
Ben <aransk22@gmail.com>
2022-10-21 23:21:47 -0400

    Created the models

--- backend/cello/models.py
+++ backend/cello/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+    date = models.DateField()
+    link = models.URLField(max_length=200)
+
+
+class ExerciseInfo(models.Model):
+    side = models.CharField(max_length=50)
+    pageAndExercise = models.IntegerField()
+    tenor = models.BooleanField()
+    treble = models.BooleanField()
+    bookID = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class Exercise(models.Model):
+    exerciseID = models.ForeignKey(ExerciseInfo, on_delete=models.CASCADE)
+    tagID = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    level = models.IntegerField()
+    tagName = models.CharField(max_length=150)
