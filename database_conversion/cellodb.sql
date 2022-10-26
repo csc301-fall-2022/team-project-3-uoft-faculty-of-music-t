531c167aba644c4f85a9fffa155ed3355ed377eb
rebplane <rebecca_nhan@rogers.com>
2022-10-26 16:29:27 -0400

    Added instructions for deployment on Heroku and sync for ID synchronization with Django tables

--- database_conversion/cellodb.sql
+++ database_conversion/cellodb.sql
@@ -1,41 +1,10 @@
--- We should only run this once, but in the case we run it again, we don't want repeat
--- information added to our database.
-DROP TABLE IF EXISTS Exercise;
-DROP TABLE IF EXISTS Tag;
-DROP TABLE IF EXISTS ExerciseInfo;
-DROP TABLE IF EXISTS Book;
-DROP TABLE IF EXISTS cello_exercise;
-DROP TABLE IF EXISTS cello_tag;
-DROP TABLE IF EXISTS cello_exerciseinfo;
-DROP TABLE IF EXISTS cello_book;
+-- On deployment, follow these steps exactly:
+-- 1) DROP the tables below or Django won't migrate the id seqs properly
+-- 2) Migrate Django tables to Postgres
+-- 3) Run this sql file (cellodb.sql) on Heroku to insert the values (do this only ONCE or we'll need to clear the tables)
+-- 4) Run syncseq.sql on Heroku to sync the sequences
 
-CREATE TABLE cello_book
-    (id INT PRIMARY KEY,
-    author VARCHAR(100),
-    publish_date VARCHAR(10),
-    title VARCHAR(300),
-    link VARCHAR(2048));
-
-CREATE TABLE cello_exerciseinfo
-    (id INT PRIMARY KEY,
-    side VARCHAR(15),
-    page_and_exercise VARCHAR(300),
-    tenor BOOLEAN,
-    treble BOOLEAN,
-    book_id INT REFERENCES cello_book);
-
-CREATE TABLE cello_tag 
-    (id INT PRIMARY KEY,
-    tag_level INT,
-    tag_name VARCHAR(100));
-
-CREATE TABLE cello_exercise
-    (id INT PRIMARY KEY,
-    exercise_id INT REFERENCES cello_exerciseinfo,
-    tag_id INT REFERENCES cello_tag,
-    UNIQUE (exercise_id, tag_id));
-
--- Load data from .csv files
+-- Insert data from .csv files
 \COPY cello_book FROM 'new_csv/book.csv' DELIMITER ',' CSV header;
 \COPY cello_exerciseinfo FROM 'new_csv/exerciseinfo.csv' DELIMITER ',' CSV header;
 \COPY cello_tag FROM 'new_csv/tag.csv' DELIMITER ',' CSV header;
---
+++ database_conversion/syncseq.sql
@@ -0,0 +1,6 @@
+-- TO BE RUN AFTER THE MIGRATION AND INSERTION OF VALUES TO HEROKU POSTGRES
+
+SELECT setval('cello_book_id_seq', max(id)) FROM cello_book;
+SELECT setval('cello_exercise_id_seq', max(id)) FROM cello_exercise;
+SELECT setval('cello_exerciseinfo_id_seq', max(id)) FROM cello_exerciseinfo;
+SELECT setval('cello_tag_id_seq', max(id)) FROM cello_tag;
