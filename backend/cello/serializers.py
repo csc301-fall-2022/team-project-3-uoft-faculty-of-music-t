6c8323e23b6f6d687fd2a53093b442fe9b300cca
Ben <aransk22@gmail.com>
2022-10-25 20:44:18 -0400

    Added the id to the fields in serializers

--- backend/cello/serializers.py
+++ backend/cello/serializers.py
@@ -4,24 +4,23 @@ from .models import Book, Exercise, ExerciseInfo, Tag
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('title', 'author', 'date', 'link')
+        fields = ('id', 'title', 'author', 'date', 'link')
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
-        fields = ('exercise_id', 'tag_id')
+        fields = ('id', 'exercise_id', 'tag_id')
 
 
 class ExerciseInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExerciseInfo
-        fields = ('exercise_id', 'tag_id')
-        fields = ('side', 'page_and_exercise', 'tenor', 'treble', 'book_id')
+        fields = ('id', 'side', 'page_and_exercise', 'tenor', 'treble', 'book_id')
 
     
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('level', 'tag_name')
+        fields = ('id', 'level', 'tag_name')
 
