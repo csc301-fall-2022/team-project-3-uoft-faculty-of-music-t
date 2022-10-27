9dfe645f3ff36254b93fe45ee4bc0b814053ab28
1moonala <junseomoon9@gmail.com>
2022-10-27 16:48:36 -0400

    Implemented basic layout of HomePage and created SearchBar components

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import "../App.css"
+import "./HomePage.css"
+import SearchBar from '../components/SearchBar'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="homePage">
+        <div className="title-container">
+            <h1 className="title">Cello Exercise Index</h1>
+        </div>
+        <div className="homePage-main-container">
+            <SearchBar />
+            <div className="content-list-container">
+                <div className="books-list-container">
+                    <h2>Books</h2>
+                </div>
+                <div className="content-list-container-divider">
+                </div>
+                <div className="browse-by-topic-list-container">
+                    <h2>Browse By Topic</h2>
+                </div>
+            </div>
+            <div className="random-exercises-container">
+                <h2>Try These Exercises!</h2>
+                <div className="random-exercises-list-container">
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
