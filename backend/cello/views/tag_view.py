3317ab424b011d7678593c9d3f6fa159aec890f1
eun-chae-s <s9710173@naver.com>
2022-10-28 15:22:39 -0400

    finish GET request for a tag at the specific level

--- backend/cello/views/tag_view.py
+++ backend/cello/views/tag_view.py
@@ -1,8 +1,17 @@
 # API endpoints here begin with /api/tag
+from msilib.schema import ServiceInstall
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from ..serializers import TagSerializer
 from ..models import Tag
 
 class TagView(viewsets.ModelViewSet):
     serializer_class = TagSerializer
-    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        queryset = Tag.objects.all()
+        level = self.request.query_params.get('level')
+        if level is not None:
+            queryset = queryset.filter(level=level)
+        return queryset
