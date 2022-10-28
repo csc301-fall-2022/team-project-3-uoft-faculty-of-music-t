a234b0c6cd44e2d00b73efa3e1937fd7f4db8c4d
eun-chae-s <s9710173@naver.com>
2022-10-28 15:38:25 -0400

    remove some unnecessary import statements

--- backend/cello/views/tag_view.py
+++ backend/cello/views/tag_view.py
@@ -1,17 +1,15 @@
 # API endpoints here begin with /api/tag
-from msilib.schema import ServiceInstall
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from ..serializers import TagSerializer
 from ..models import Tag
 
 class TagView(viewsets.ModelViewSet):
     serializer_class = TagSerializer
-
+    
     def get_queryset(self):
         queryset = Tag.objects.all()
         level = self.request.query_params.get('level')
         if level is not None:
             queryset = queryset.filter(level=level)
         return queryset
+        
