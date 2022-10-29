0dac69c85c1c387a4b2282aaa80e39e0237748da
Eunchae Seong <83269618+eun-chae-s@users.noreply.github.com>
2022-10-28 23:06:32 -0400

    Revert "Fixed issue with columns being in the wrong order in the models"

--- backend/cello/migrations/0001_initial.py
+++ backend/cello/migrations/0001_initial.py
@@ -16,9 +16,9 @@ class Migration(migrations.Migration):
             name='Book',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
                 ('author', models.CharField(max_length=150)),
                 ('date', models.DateField()),
-                ('title', models.CharField(max_length=200)),
                 ('link', models.URLField()),
             ],
         ),
