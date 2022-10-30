3ed269130fbd78fa99be064191387451aaa4f782
Ben <aransk22@gmail.com>
2022-10-30 10:31:55 -0400

    created pages for the ExerciseInfoView, TagView, TagByLevelView, and BookView

--- backend/cello/views/book_view.py
+++ backend/cello/views/book_view.py
@@ -2,8 +2,11 @@
 from rest_framework import viewsets
 from ..serializers import BookSerializer
 from ..models import Book
+from cello.pagination import StandardResultsSetPagination
 
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset= Book.objects.all()
+    pagination_class = StandardResultsSetPagination
+    
