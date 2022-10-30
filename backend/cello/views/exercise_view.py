3d28647ecb62faaf84bcdb934621ca864252c978
Ben <aransk22@gmail.com>
2022-10-30 02:55:27 -0400

    First attempt at using pagination to limit how many exercises are shown

--- backend/cello/views/exercise_view.py
+++ backend/cello/views/exercise_view.py
@@ -1,9 +1,12 @@
 # API endpoints here begin with /api/exercise
 from rest_framework import viewsets
+
+from cello.pagination import StandardResultsSetPagination
 from ..serializers import ExerciseSerializer
 from ..models import Exercise
 
 
 class ExerciseView(viewsets.ModelViewSet):
     serializer_class = ExerciseSerializer
-    queryset= Exercise.objects.all()
+    queryset = Exercise.objects.all()
+    pagination_class = StandardResultsSetPagination
