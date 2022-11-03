8f809fd27e588fd9b16a62a500a7d874399756af
rebplane <rebecca_nhan@rogers.com>
2022-11-02 22:01:27 -0400

    Fixed overflow of book container and trimmed book title/author if too long

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -3,13 +3,18 @@ import './Books.css'
 import { Link } from "react-router-dom";
 
 export default function Books({ book }) {
+
+  // Trim title and author if too long
+  const title = book.title.length > 50 ? book.title.substring(0, 50)+"..." : book.title
+  const author = book.author.length > 20 ? book.author.substring(0, 20) + "..." : book.author;
+
   return (
     <div className='book-container'>
         {/* TODO: Get Book info from api */}
         <Link to="bookDetails" state={{ id: book.id }}> 
-            <p>{book.title}</p>
+            <p>{title}</p>
         </Link>
-        <p>{book.author} ({book.date})</p>
+        <p> {author} ({book.date})</p>
     </div>
   )
 }
