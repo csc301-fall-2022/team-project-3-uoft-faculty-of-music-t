88fc50a933a5517820412e0a48a41a13bb72064a
rebplane <rebecca_nhan@rogers.com>
2022-11-03 16:45:27 -0400

    Edited urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cello.urls', namespace='api')),
-    re_path('.*', TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html")),
 ]
