1704751e6e96694cf21a286f7026186f29ca1383
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-03 17:00:54 -0400

    Added one layer of parent to BASE_DIR

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ import django_on_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
