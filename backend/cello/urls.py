176f14bd0565ee6e21a505c0a9feb567ea49fc1c
erenfn <81182796+erenfn@users.noreply.github.com>
2022-11-03 02:53:58 -0400

    Update urls.py
    Again, I added the urls for other ExerciseInfoViews. They may be unneccesary so lmk, if I should delete them

--- backend/cello/urls.py
+++ backend/cello/urls.py
@@ -4,7 +4,8 @@ from rest_framework import routers
 from cello.views.author_view import AuthorView
 from cello.views.book_view import BookView
 from cello.views.exercise_view import ExerciseView
-from cello.views.exerciseinfo_view import ExerciseInfoView
+from cello.views.exerciseinfo_view import ExerciseInfoView, ExerciseByAuthorView, \
+    ExerciseByBookView, ExerciseByTagLevelView, ExerciseByTagNameOrView, ExerciseByFilterView
 from cello.views.tag_view import TagView, TagByExerciseView, TagByLevelView
 
 from rest_framework_simplejwt.views import (
@@ -23,6 +24,17 @@ router.register(r'authors', AuthorView, 'authors')
 urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('tag/exercise/<int:exercise_id>', TagByExerciseView.as_view({'get': 'list'}), name='tag-by-exercise'),
-    path('tag/level/<int:level_num>',TagByLevelView.as_view({'get': 'list'}), name='tag-by-level')
+    path('tag/exercise/<int:exercise_id>', TagByExerciseView.as_view({'get': 'list'}),
+         name='tag-by-exercise'),
+    path('tag/level/<int:level_num>', TagByLevelView.as_view({'get': 'list'}), name='tag-by-level'),
+    path('exerciseinfo/author/<str:author>', ExerciseByAuthorView.as_view({'get': 'list'}),  #we don't need these actually
+         name='exercise-by-author'),
+    path('exerciseinfo/book/<str:title>', ExerciseByBookView.as_view({'get': 'list'}),
+         name='exercise-by-book'),
+    path('exerciseinfo/tag/level/<int:level_num>', ExerciseByTagLevelView.as_view({'get': 'list'}),
+         name='exercise-by-tag-level'),
+    path('exerciseinfo/tag/name_or/<str:tag_name>',
+         ExerciseByTagNameOrView.as_view({'get': 'list'}), name='exercise-by-tag-name')
+
+
 ]
