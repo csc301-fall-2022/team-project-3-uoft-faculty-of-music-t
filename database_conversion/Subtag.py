ed385fbea35aaf6ed09ff337a1a801a2266674b3
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-17 23:47:43 -0500

    Created Subtag csv where we store parent and children tags

---
+++ database_conversion/Subtag.py
@@ -0,0 +1,5 @@
+class Subtag:
+    def __init__(self, tag_id, parent_tag, child_tag):
+        self.id = tag_id
+        self.parent_id = parent_tag
+        self.child_id = child_tag
--- database_conversion/create_modified_csv.py
+++ database_conversion/create_modified_csv.py
@@ -9,6 +9,7 @@ from Book import Book
 from ExerciseInfo import ExerciseInfo
 from Tag import Tag
 from Exercise import Exercise
+from Subtag import Subtag
 
 
 # Reads the books from the original_books.csv file and returns a list of books
@@ -118,6 +119,55 @@ def extract_tags_and_exercises():
     return tags_objects, exercise_with_tags
 
 
+def extract_subtags():
+    with open('old_csv/original_exercises.csv', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        tags_list = []
+        tags_objects = []  # A list of Tag objects
+        subtag_list = []
+        subtag_objects = [] # A list of Subtag objects
+        tag_id = 1 # Initialize tag id
+        subtag_id = 1 # Initialize subtag ID
+
+        for row in reader:
+            # If the row is empty, skip the line
+            if row[0] == '':
+                continue
+
+            level1_name = row[1]
+            level2_name = row[2]
+            level3_name = row[3]
+
+            levels = [level1_name, level2_name, level3_name]
+
+            # For each level specificity, adds the tag name and its specificity level if it's not already in
+            # the tags list to the tags_objects list. The exercise is also added with the tag to the
+            # exercise_with_tags list
+            for i in range(0, 3):
+                # Create the Tags
+                if levels[i] != "" and levels[i] not in tags_list:
+                    tags_list.append(levels[i])
+                    tags_objects.append(Tag(tag_id, i + 1, levels[i]))
+                    tag_id += 1
+                else:
+                    for tag in tags_objects:
+                        if levels[i] == tag.tag_name:
+                            break
+
+                # Create the Subtags
+                if i != 0 and levels[i] != "":
+                    parent_tag = levels[i-1]
+                    child_tag = levels[i]
+                    if (parent_tag, child_tag) not in subtag_list:
+                        subtag_list.append((parent_tag, child_tag))
+                        # Get parent_tag id and child_tag id
+                        parent_tag_id = [tag.tag_id for tag in tags_objects if tag.tag_name == parent_tag][0]
+                        child_tag_id = [tag.tag_id for tag in tags_objects if tag.tag_name == child_tag][0]
+                        subtag_objects.append(Subtag(subtag_id, parent_tag_id, child_tag_id))
+                        subtag_id += 1
+    return subtag_objects
+
+
 # Creates the tag.csv file
 def create_tags(tags):
     with open('new_csv/tag.csv', 'w', encoding='utf-8', newline='') as f:
@@ -137,8 +187,14 @@ def create_exercise(exercises):
             writer.writerow([count, ex.exercise_id, ex.tag_id])
             count += 1
 
+def create_subtag_csv(subtags):
+    with open('new_csv/subtag.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'parent_id', 'child_id'])
+        for tag in subtags:
+            writer.writerow([tag.id, tag.parent_id, tag.child_id])
 
-if __name__ == "__main__":
+def old_models():
     # Extract and create books csv
     extracted_books = extract_books()
     create_book_csv(extracted_books)
@@ -162,3 +218,9 @@ if __name__ == "__main__":
     create_tags(extracted_tags_and_exercises[0])
     # Create exercise list pairing each exercise with its tags
     create_exercise(extracted_tags_and_exercises[1])
+
+if __name__ == "__main__":
+    # Extract subtags and create subtag csv
+    extracted_subtags = extract_subtags()
+    create_subtag_csv(extracted_subtags)
+    
