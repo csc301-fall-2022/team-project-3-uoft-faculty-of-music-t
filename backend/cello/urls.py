9b227486c774f276e2772fb985eedb3df1ceb626
erenfn <81182796+erenfn@users.noreply.github.com>
2022-11-17 19:10:32 -0500

    Update urls.py

--- backend/cello/urls.py
+++ backend/cello/urls.py
@@ -4,8 +4,7 @@ from rest_framework import routers
 from cello.views.author_view import AuthorView
 from cello.views.book_view import BookView
 from cello.views.exercise_view import ExerciseView
-from cello.views.exerciseinfo_view import ExerciseInfoView, ExerciseByAuthorView, \
-    ExerciseByBookView, ExerciseByTagLevelView, ExerciseByTagNameOrView, ExerciseByFilterView
+from cello.views.exerciseinfo_view import ExerciseInfoView
 from cello.views.tag_view import TagView, TagByExerciseView, TagByLevelView
 
 from rest_framework_simplejwt.views import (
@@ -26,15 +25,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('tag/exercise/<int:exercise_id>', TagByExerciseView.as_view({'get': 'list'}),
          name='tag-by-exercise'),
-    path('tag/level/<int:level_num>', TagByLevelView.as_view({'get': 'list'}), name='tag-by-level'),
-    path('exerciseinfo/author/<str:author>', ExerciseByAuthorView.as_view({'get': 'list'}),  #we don't need these actually
-         name='exercise-by-author'),
-    path('exerciseinfo/book/<str:title>', ExerciseByBookView.as_view({'get': 'list'}),
-         name='exercise-by-book'),
-    path('exerciseinfo/tag/level/<int:level_num>', ExerciseByTagLevelView.as_view({'get': 'list'}),
-         name='exercise-by-tag-level'),
-    path('exerciseinfo/tag/name_or/<str:tag_name>',
-         ExerciseByTagNameOrView.as_view({'get': 'list'}), name='exercise-by-tag-name')
-
-
+    path('tag/level/<int:level_num>', TagByLevelView.as_view({'get': 'list'}), name='tag-by-level')
 ]
