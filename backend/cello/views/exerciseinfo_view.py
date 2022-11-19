7e5a131cf85b36872fa2ba5348b7990328f741a5
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-19 15:06:02 -0500

    Added Eren's filter code

--- backend/cello/views/exerciseinfo_view.py
+++ backend/cello/views/exerciseinfo_view.py
@@ -2,8 +2,9 @@
 from rest_framework import viewsets
 from cello.pagination import StandardResultsSetPagination
 from ..serializers import ExerciseInfoSerializer
-from ..models import ExerciseInfo
+from ..models import ExerciseInfo, Book
 from collections import defaultdict
+from django.db.models import Q
 
 
 class ExerciseInfoView(viewsets.ModelViewSet):
@@ -11,5 +12,25 @@ class ExerciseInfoView(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
-        queryset= ExerciseInfo.objects.all().order_by('id')
+
+        tag_id = self.request.query_params.getlist('tag_id')
+        author = self.request.query_params.getlist('author')
+        book_id = self.request.query_params.getlist('book_id')
+
+        if tag_id:
+            exercises = ExerciseInfo.objects.filter(tags__in=tag_id)
+        else:
+            exercises = ExerciseInfo.objects.all()
+        if author:
+            if book_id:
+                books = Book.objects.filter(Q(id__in=book_id) | Q(author__in=author))
+            else:
+                books = Book.objects.filter(author__in=author)
+        else:
+            if book_id:
+                books = Book.objects.filter(id__in=book_id)
+            else:
+                books = Book.objects.all()
+
+        queryset = ExerciseInfo.objects.filter(id__in=exercises, book_id__in=books)
         return queryset
