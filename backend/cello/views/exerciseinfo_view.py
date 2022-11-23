52201064e5f2004d3ae07b7fa35e21195dc8a931
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-23 00:48:58 -0500

    Added search parameter to exercise info endpoint

--- backend/cello/views/exerciseinfo_view.py
+++ backend/cello/views/exerciseinfo_view.py
@@ -2,7 +2,7 @@
 from rest_framework import viewsets
 from cello.pagination import StandardResultsSetPagination
 from ..serializers import ExerciseInfoSerializer
-from ..models import ExerciseInfo, Book
+from ..models import ExerciseInfo, Book, Tag
 from collections import defaultdict
 from django.db.models import Q
 
@@ -16,6 +16,7 @@ class ExerciseInfoView(viewsets.ModelViewSet):
         tag_id = self.request.query_params.getlist('tag_id')
         author = self.request.query_params.getlist('author')
         book_id = self.request.query_params.getlist('book_id')
+        search = self.request.query_params.get('search')
 
         if tag_id:
             exercises = ExerciseInfo.objects.filter(tags__in=tag_id)
@@ -33,4 +34,18 @@ class ExerciseInfoView(viewsets.ModelViewSet):
                 books = Book.objects.all()
 
         queryset = ExerciseInfo.objects.filter(id__in=exercises, book_id__in=books)
+
+        # Searches to find a match for the search term within author, tag name, exercise name, and book name
+        if search:
+            # Book title match
+            book_title_matches = Book.objects.filter(title__icontains=search)
+            # Author match
+            author_matches = Book.objects.filter(author__icontains=search)
+            # Exercise title match
+            exercise_title_match = ExerciseInfo.objects.filter(page_and_exercise__icontains=search)
+            # Tag name match
+            tag_matches = Tag.objects.filter(tag_name__icontains=search)
+            queryset = queryset.filter(Q(book_id__in=book_title_matches) | Q(book_id__in=author_matches) | \
+                Q(tags__in=tag_matches) | Q(id__in=exercise_title_match))
+
         return queryset
