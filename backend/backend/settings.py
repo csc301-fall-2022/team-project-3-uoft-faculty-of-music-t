90a48ca598a55224630ef0bb1f1bb1eb67af0df3
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-23 01:11:41 -0500

    Added drf_yasg documentation library to project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'drf_yasg',
     'cello',
 ]
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Documentation schema
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cello.urls', namespace='api')),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html")),
 ]
