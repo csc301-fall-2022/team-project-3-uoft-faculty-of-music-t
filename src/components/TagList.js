af284208b548f1c8d2f6b98d61c6b3d30009e260
eun-chae-s <s9710173@naver.com>
2022-11-25 17:12:07 -0500

    finished deleting tags function

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -2,19 +2,24 @@ import React from "react";
 import "./TagList.css";
 
 export default function TagList({ tags, setSelectedTags }) {
-  // on click => delete the tags
   const deleteTag = (e) => {
-    console.log(e.target.value);
     e.preventDefault();
-
-    setSelectedTags(tags.filter((tag) => tag["tag_name"] !== e.target.value));
+    const targetValue = e.target.innerHTML.substr(
+      0,
+      e.target.innerHTML.indexOf(" ✖️")
+    );
+    console.log(targetValue);
+    setSelectedTags(tags.filter((tag) => tag["tag_name"] !== targetValue));
   };
   return (
     <div className="tags-container">
       {tags.map((tag) => {
         return (
-          <button className={"tag-icon" + tag["level"]} onClick={deleteTag}>
-            {tag["tag_name"]} {"⨉"}
+          <button
+            className={"tag-icon" + tag["level"]}
+            onClick={(e) => deleteTag(e)}
+          >
+            {tag["tag_name"]} {"✖️"}
           </button>
         );
       })}
