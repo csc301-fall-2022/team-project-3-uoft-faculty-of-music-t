be4067b0c1e01b647c39c0c93e5c8297e73e485c
eun-chae-s <s9710173@naver.com>
2022-11-26 13:39:43 -0500

    remove trailing slash

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,103 +1,86 @@
-import axios from 'axios';
+import axios from "axios";
 
-const server_url = "https://cello-exercise-index.herokuapp.com/"
+// const server_url = "https://cello-exercise-index.herokuapp.com/";
+const server_url = "http://127.0.0.1:8000/";
 
 /** requests and sets list of books according to the passed in setter*/
 export function getAllBooks(setBooks) {
-    axios
-    .get(server_url + "api/book/")
-    .then((res) => {
-      setBooks(res.data.results);
-    });
+  axios.get(server_url + "api/book/").then((res) => {
+    setBooks(res.data.results);
+  });
 }
 
 /** requests and sets book details according to the passed in setter and book id*/
 export function getBookDetails(setBookDetails, id) {
-    axios
-    .get( `${server_url}api/book/${id}/` )
-    .then((res) => {
-      setBookDetails(res.data);
-    });
+  axios.get(`${server_url}api/book/${id}/`).then((res) => {
+    setBookDetails(res.data);
+  });
 }
 
 /** requests and sets list of exercises according to the passed in setter*/
 export function getAllExercises(setExercises) {
-    axios
-    .get(server_url + "api/exerciseinfo/")
-    .then((res) => {
-      setExercises(res.data.results);
-    });
+  axios.get(server_url + "api/exerciseinfo/").then((res) => {
+    setExercises(res.data.results);
+  });
 }
 
 /** requests and sets exercise details according to the passed in setter and exercise id*/
 export function getExerciseDetails(setExerciseDetails, id) {
-    axios
-    .get( `${server_url}api/exerciseinfo/${id}/` )
-    .then((res) => {
-      setExerciseDetails(res.data);
-    });
+  axios.get(`${server_url}api/exerciseinfo/${id}/`).then((res) => {
+    setExerciseDetails(res.data);
+  });
 }
 
 export function getAllTags(setTopics) {
-  axios
-  .get( `${server_url}api/tag/` )
-  .then((res) => {
+  axios.get(`${server_url}api/tag/`).then((res) => {
     setTopics(res.data.results);
   });
 }
 
 export function getTagsByLevel(level) {
   return new Promise((resolve, reject) => {
-    axios
-    .get(`${server_url}api/tag/level/${level}/`)
-    .then((res) => {
-      resolve(res.data.results)
-    })
-  })
+    axios.get(`${server_url}api/tag/level/${level}/`).then((res) => {
+      resolve(res.data.results);
+    });
+  });
 }
 
 export function getSubTagsByTag(id) {
   return new Promise((resolve, reject) => {
-    axios
-    .get(`${server_url}api/tag/subtag/${id}/`)
-    .then((res) => {
-      resolve(res.data.results)
-    })
-  })
+    axios.get(`${server_url}api/tag/subtag/${id}/`).then((res) => {
+      resolve(res.data.results);
+    });
+  });
 }
 
 /** requests and sets exercises according to the passed in setter and book id */
 export function getExerciseByBook(setExercises, id) {
-  axios
-    .get( `${server_url}api/exerciseinfo/?book_id=${id}/` )
-    .then((res) => {
-      setExercises(res.data.results);
-    });
+  axios.get(`${server_url}api/exerciseinfo/?book_id=${id}`).then((res) => {
+    setExercises(res.data.results);
+  });
 }
 
 export function getExerciseByTags(setExercises, tags, bookId) {
   if (Object.keys(tags).length === 0) {
-    return
+    return;
   }
 
-  let i = 0
-  let paramsEndpoint = ""
+  let i = 0;
+  let paramsEndpoint = "";
   for (const tag in tags) {
     if (i === 0) {
-      paramsEndpoint += "?tag_id=" + tags[tag]
+      paramsEndpoint += "?tag_id=" + tags[tag];
     } else {
-      paramsEndpoint += "&tag_id=" + tags[tag]
+      paramsEndpoint += "&tag_id=" + tags[tag];
     }
-    i += 1
+    i += 1;
   }
 
   if (bookId !== "") {
-    paramsEndpoint += "&book_id=" + bookId 
+    paramsEndpoint += "&book_id=" + bookId;
   }
 
-  axios
-    .get( `${server_url}api/exerciseinfo/${paramsEndpoint}/` )
-    .then((res) => {
-      setExercises(res.data.results);
-    });
+  axios.get(`${server_url}api/exerciseinfo/${paramsEndpoint}/`).then((res) => {
+    setExercises(res.data.results);
+  });
 }
