0c964afcad8c5a97ceba5397c0b66489783ba29a
hMarc16 <mark.hu@mail.utoronto.ca>
2022-12-06 12:15:14 -0500

    initial commit login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchPage from "./pages/SearchPage";
 import BookDetailsPage from "./pages/BookDetailsPage";
 import ExerciseDetailsPage from "./pages/ExerciseDetailsPage";
 import EditExercisePage from "./pages/EditExercisePage";
+import LoginPage from "./pages/LoginPage";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="bookDetails" element={<BookDetailsPage />}></Route>
         <Route path="exerciseDetails" element={<ExerciseDetailsPage />}></Route>
         <Route path="editExercise" element={<EditExercisePage />}></Route>
+        <Route path="login" element={<LoginPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
