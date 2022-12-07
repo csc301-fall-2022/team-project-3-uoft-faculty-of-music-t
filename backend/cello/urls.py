4e2cac8c8517d7cbc9e67bb35d16ce3672d6ea4b
erenfn <81182796+erenfn@users.noreply.github.com>
2022-12-07 02:32:32 -0500

    Update urls.py

--- backend/cello/urls.py
+++ backend/cello/urls.py
@@ -5,6 +5,7 @@ from cello.views.author_view import AuthorView
 from cello.views.book_view import BookView
 from cello.views.exerciseinfo_view import ExerciseInfoView, ExerciseRandomView
 from cello.views.tag_view import TagView, TagByExerciseView, TagByLevelView, SubtagView
+from cello.views.exercise_request import EditExerciseRequestView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -17,13 +18,16 @@ router.register(r'exerciseinfo', ExerciseInfoView, 'exerciseinfo')
 router.register(r'book', BookView, 'book')
 router.register(r'tag', TagView, 'tag')
 router.register(r'authors', AuthorView, 'authors')
+router.register(r'requested', EditExerciseRequestView, 'requested')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('tag/exercise/<int:exercise_id>/', TagByExerciseView.as_view({'get': 'list'}), name='tag-by-exercise'),
-    path('tag/level/<int:level_num>/',TagByLevelView.as_view({'get': 'list'}), name='tag-by-level'),
+    path('tag/level/<int:level_num>/', TagByLevelView.as_view({'get': 'list'}), name='tag-by-level'),
     path('tag/subtag/<int:tag_id>/', SubtagView.as_view({'get': 'list'}, name='subtag-view')),
     path('exerciseinfo/exercises/random/', ExerciseRandomView.as_view(), name='exercises-random'),
-    path('tag/tags/random/', ExerciseRandomView.as_view(), name='exercises-random')
+    path('tag/tags/random/', ExerciseRandomView.as_view(), name='exercises-random'),
+    path('requested/approve/<int:request_id>', EditExerciseRequestView.as_view({'get': 'retrieve'}), name='approve-edit-exercise-request'),
+    path('requested/reject/<int:request_id>', EditExerciseRequestView.as_view({'get': 'retrieve'}), name='reject-edit-exercise-request')
 ]
