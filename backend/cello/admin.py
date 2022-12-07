0dace81a42277c31aaeb43b1410d9cb5248c5581
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-12-07 04:11:04 -0500

    Added more descriptive admin panel names

--- backend/cello/admin.py
+++ backend/cello/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import ExerciseInfo, Tag, Book
+from .models import ExerciseInfo, Tag, Book, Subtag
 
 # Register your models here.
 admin.site.register(ExerciseInfo)
 admin.site.register(Tag)
 admin.site.register(Book)
-
+admin.site.register(Subtag)
