e05bacc556a8a02935dd57990bdf20b7134300d2
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-12-09 17:35:12 -0500

    Removed EditExerciseRequest from admin panel (we use frontend for this instead)

--- backend/cello/admin.py
+++ backend/cello/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import ExerciseInfo, Tag, Book, EditExerciseRequest, Subtag
+from .models import ExerciseInfo, Tag, Book, Subtag
 
 # Register your models here.
 admin.site.register(ExerciseInfo)
 admin.site.register(Tag)
 admin.site.register(Book)
-admin.site.register(EditExerciseRequest)
 admin.site.register(Subtag)
