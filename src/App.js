cc4fc9e16b6ae0599a429baf881d57ac0d20f932
eun-chae-s <s9710173@naver.com>
2022-12-09 23:06:52 -0500

    add protected layer on request page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import BookDetailsPage from "./pages/BookDetailsPage";
@@ -9,8 +9,22 @@ import LoginPage from "./pages/LoginPage";
 import RequestsPage from "./pages/RequestsPage";
 import RequestDetailPage from "./pages/RequestDetailPage";
 import { SearchProvider } from "./contexts/SearchContext";
+import { createContext, useState } from "react";
+
+const ProtectedRoute = ({ children }) => {
+  let flag = false;
+
+  localStorage.getItem("admin") ? (flag = true) : (flag = false);
+
+  if (!flag) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
     <BrowserRouter>
       <SearchProvider>
@@ -23,11 +37,27 @@ function App() {
             element={<ExerciseDetailsPage />}
           ></Route>
           <Route path="editExercise" element={<EditExercisePage />}></Route>
-          <Route path="login" element={<LoginPage />}></Route>
-          <Route path="requested" element={<RequestsPage />}></Route>
-          <Route path="requestDetail" element={<RequestDetailPage />}></Route>
         </Routes>
       </SearchProvider>
+      <Routes>
+        <Route path="login" element={<LoginPage />}></Route>
+        <Route
+          path="requested"
+          element={
+            <ProtectedRoute>
+              <RequestsPage />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="requestDetail"
+          element={
+            <ProtectedRoute>
+              <RequestDetailPage />
+            </ProtectedRoute>
+          }
+        ></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
