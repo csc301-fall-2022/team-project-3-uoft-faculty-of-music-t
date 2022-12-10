c26f8280b8285c6fa32aaf9ff34ae91a0a7ddc40
hMarc16 <mark.hu@mail.utoronto.ca>
2022-12-10 12:44:14 -0500

    logout button done

--- src/pages/RequestsPage.js
+++ src/pages/RequestsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 import "./RequestsPage.css";
+import Logout from "../components/Logout"
 import Tabs from "../components/Tabs";
 import { Link } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function AdminPage() {
           <h1 className="title">Cello Exercise Index</h1>
         </Link>
         <h2 className="requests-welcome">Hello, Admin!</h2>
+        <Logout />
       </div>
       <Tabs />
     </div>
